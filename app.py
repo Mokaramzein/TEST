300d3ce508c5a3afcddeffcf1a819e8c3c01f7f8
Mokaramzein <mokaramalzein26@gmail.com>
2024-06-24 23:23:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,12 @@
 
-from flask import Flask, render_template_string, request
+from flask import Flask, render_template_string,render_template, request
 import subprocess
 
 app = Flask(__name__)
 
-# HTML template with a button to trigger the PowerShell command
-HTML_TEMPLATE = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Execute PowerShell Command</title>
-</head>
-<body>
-    <h1>Execute PowerShell Command</h1>
-    <form method="post" action="/execute">
-        <button type="submit">Run Command</button>
-    </form>
-    {% if output %}
-    <h2>Command Output:</h2>
-    <pre>{{ output }}</pre>
-    {% endif %}
-</body>
-</html>
-"""
-
 @app.route('/')
 def index():
-    return render_template_string(HTML_TEMPLATE)
+    return render_template(index.html)
 
 @app.route('/execute', methods=['POST'])
 def execute():
